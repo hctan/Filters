4d1cb8798b96c8d89394d0a8d7bf4a08ee1d3ceb
Han Chong <hanchong@senatraffic.com.my>
2016-02-11 13:37:09 +0800

    Add view and test custom authorization attribute

---
+++ Filters/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Filters.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Filters.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        [CustomAuth(false)]
+        public ActionResult Index()
+        {
+            ViewBag.Message = "This is the Index action on the Home controller";
+            return View();
+        }
+    }
+}
